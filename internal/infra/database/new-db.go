2ecd278e336347b097c937a2a0e4ab1efd1ad33d
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2023-11-28 22:30:28 -0300

    chore: :sparkles: create migrations using struct notation

--- internal/infra/database/new-db.go
+++ internal/infra/database/new-db.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"github.com/FelipeBelloDultra/emailn/internal/domain/campaign"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -12,6 +13,11 @@ func NewDb() *gorm.DB {
 	if err != nil {
 		panic(("fail to connect to database"))
 	}
+
+	db.AutoMigrate(
+		&campaign.Campaign{},
+		&campaign.Contact{},
+	)
 
 	return db
 }
