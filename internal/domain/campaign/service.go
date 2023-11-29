d1a30779f75263fa5f4d754e19270e23b5092810
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2023-11-29 11:44:16 -0300

    fix: :bug: remove cancel endpoint

--- internal/domain/campaign/service.go
+++ internal/domain/campaign/service.go
@@ -14,7 +14,6 @@ type ServiceImp struct {
 type Service interface {
 	Create(newcampaign contract.NewCampaign) (string, error)
 	GetBy(id string) (*contract.CampaignResponse, error)
-	Cancel(id string) error
 	Delete(id string) error
 }
 
@@ -46,26 +45,6 @@ func (s *ServiceImp) GetBy(id string) (*contract.CampaignResponse, error) {
 		Status:               campaign.Status,
 		AmountOfEmailsToSend: len(campaign.Contacts),
 	}, nil
-}
-
-func (s *ServiceImp) Cancel(id string) error {
-	campaign, err := s.Repository.GetBy(id)
-	if err != nil {
-		return internalerrors.ProcessErrorToReturn(err)
-	}
-
-	if campaign.Status != Pending {
-		return errors.New("Campaign status invalid")
-	}
-
-	campaign.Cancel()
-
-	err = s.Repository.Update(campaign)
-	if err != nil {
-		return internalerrors.ErrInternal
-	}
-
-	return nil
 }
 
 func (s *ServiceImp) Delete(id string) error {
